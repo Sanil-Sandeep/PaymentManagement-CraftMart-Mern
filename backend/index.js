8bca32efd97b8aab0e6c713ae85663b5b9343c37
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-22 20:40:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,39 @@ import mongoose from "mongoose";
 import cors from 'cors';
 
 
+import { Payment } from './models/paymentModel.js';
+
+import paymentsRoutes from './routes/paymentsRoute.js';
+import paymentExpensesRoute from './routes/paymentExpensesRoute.js'
+
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//Option 01: Allow all origins with Default of cors(*)
+app.use(cors());
+
+//Option 02: Allow custom origins
+/*
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+*/
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome my project');
+});
+
+app.use('/payments', paymentsRoutes);
+app.use('/expenses', paymentExpensesRoute);
 
 mongoose
   .connect(mongoDBURL)
