1cc95638e726df89e5adfd938493724d0388146c
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-24 18:13:08 +0530

    Add route for update a payment

--- backend/routes/paymentsRoute.js
+++ backend/routes/paymentsRoute.js
@@ -72,6 +72,42 @@ router.get("/:id", async (request, response) => {
   }
 });
 
+//Route for update a payment
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+    //!request.body.paymentID ||
+      !request.body.productName ||
+      !request.body.price ||
+      !request.body.quantity ||
+      !request.body.totalPrice ||
+      !request.body.cardHolderName ||
+      !request.body.cardNumber ||
+      !request.body.cvv ||
+      !request.body.email
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: paymentID, productName, price, quantity, totalPrice, cardHolderName, cardNumber, cvv, email",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Payment.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Payment not found" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Payment details updated successdully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 
 
 
