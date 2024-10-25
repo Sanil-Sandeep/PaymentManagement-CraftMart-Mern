6e75d93c78716edf5937ee04b6c5c5c7fa354110
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-25 16:03:29 +0530

    add route for save a expense

--- backend/routes/paymentExpensesRoute.js
+++ backend/routes/paymentExpensesRoute.js
@@ -3,6 +3,36 @@ import { Expenses } from '../models/paymentExpensesModel.js';
 
 const router = express.Router();
 
+//Route for save a expense
+router.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.eID ||
+      !request.body.name ||
+      !request.body.expense ||
+      !request.body.cost 
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: eID, name, expense, cost",
+      });
+    }
+    const newExpense = {
+      eID: request.body.eID,
+      name: request.body.name,
+      expense: request.body.expense,
+      cost: request.body.cost,
+    };
+
+    const expense = await Expenses.create(newExpense);
+
+    return response.status(201).send(expense);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 
 
 export default router;
