5367adff2fdf646d33d3538c3b814428187bc186
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-30 10:04:46 +0530

    Create DeleteExpense.jsx

---
+++ frontend/src/pages/PaymentManagement/Payment/DeleteExpense.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import BackButton from '../../../components/BackButton';
+import Spinner from '../../../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import Header from '../../../components/headerfooter/Header';
+import Footer from '../../../components/headerfooter/Footer';
+
+const DeleteExpense = () => {
+  
+
+export default DeleteExpense;
