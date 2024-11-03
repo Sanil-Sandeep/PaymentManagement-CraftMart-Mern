713e149614065d27b8e9af2e42bc2e327d57b49b
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-03 09:29:40 +0530

    Create EditPayment.jsx

---
+++ frontend/src/pages/PaymentManagement/Payment/EditPayment.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import BackButton from '../../../components/BackButton';
+import Spinner from '../../../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import visaImg from '../../../components/images/visa.png';
+import masterImg from '../../../components/images/master.png'; 
+import { useSnackbar } from 'notistack';
+import Header from '../../../components/headerfooter/Header';
+import Footer from '../../../components/headerfooter/Footer';
+
+function EditPayment() {
+
+
+
+export default EditPayment;
