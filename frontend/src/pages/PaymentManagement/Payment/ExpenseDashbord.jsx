e5382093124d14b4bfbd106f23bfbfc9ec8ebfdf
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-04 21:56:30 +0530

    Create ExpenseDashbord.jsx

---
+++ frontend/src/pages/PaymentManagement/Payment/ExpenseDashbord.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../../../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import Header from '../../../components/headerfooter/Header';
+import Footer from '../../../components/headerfooter/Footer';
+import logo from '../../../images/logo.png';
+
+const ExpensesDashboard = () => {
+  
+
+export default ExpensesDashboard;
