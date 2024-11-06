45f401cf09de8f888e9ebc949858a8eb84d7ca6e
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-06 10:22:22 +0530

    add css for PaymentDashbord.jsx

--- frontend/src/pages/PaymentManagement/Payment/PaymentDashbord.jsx
+++ frontend/src/pages/PaymentManagement/Payment/PaymentDashbord.jsx
@@ -265,7 +265,208 @@ const PaymentDashboard = () => {
   );
 };
 
+// CSS styles
+const styles = {
+  page: {
+    backgroundColor: '#fff',
+    minHeight: '130vh',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: '1rem',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  container: {
+    width: '100%',
+    maxWidth: '1200px',
+    margin: '0 auto',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontFamily: 'Poppins, sans-serif',
+    color: '#330D0F', 
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginBottom: '100px',
+    marginTop: '30px',
+},
+buttonexpense: {
+  backgroundColor: '#330D0F',
+  color: '#F1EEDA',
+  width: '150px',
+  height: '50px',
+  padding: '10px 20px',
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  fontFamily: 'Poppins, sans-serif',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  border: '2px solid #330D0F',
+  transition: 'background-color 0.3s ease, border-color 0.3s ease',
+  marginLeft: '300px',
+},
+buttonincome: {
+  backgroundColor: '#330D0F',
+  color: '#F1EEDA',
+  width: '150px',
+  height: '50px',
+  padding: '10px 20px',
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  fontFamily: 'Poppins, sans-serif',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  border: '2px solid #330D0F',
+  transition: 'background-color 0.3s ease, border-color 0.3s ease',
+  marginLeft: '50px',
+},
+buttonprofit: {
+  backgroundColor: '#330D0F',
+  color: '#F1EEDA',
+  width: '150px',
+  height: '50px',
+  padding: '10px 20px',
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  fontFamily: 'Poppins, sans-serif',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  border: '2px solid #330D0F',
+  transition: 'background-color 0.3s ease, border-color 0.3s ease',
+  marginLeft: '50px',
+},
+  title: {
+    fontSize: '1.875rem',
+    margin: '2rem 0',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  downloadButton: {
+    marginBottom: '10px',
+    backgroundColor: '#330D0F',
+    borderColor: '#330D0F',
+    color: '#fff', // Text color
+    padding: '0.5rem 1rem',
+    fontSize: '0.88rem',
+    fontWeight: 'bold',
+    borderRadius: '0.25rem',
+    cursor: 'pointer',
+    border: '2px solid #330D0F',
+    fontFamily: 'Poppins, sans-serif',
+    transition: 'background-color 0.3s, border-color 0.3s',
+  },
+  downloadButtonHover: {
+    backgroundColor: '#4a1a1b', // Darker shade for hover
+    borderColor: '#4a1a1b',
+  },
+  addIcon: {
+    fontSize: '2rem',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  searchContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: '1rem',
+  },
+  searchInput: {
+    width: '300px',
+    padding: '0.5rem',
+    fontSize: '0.8rem',
+    borderRadius: '0.25rem',
+    border: '3px solid #330D0F',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  searchButton: {
+    marginLeft: '0.5rem',
+    padding: '0.5rem 1rem',
+    fontSize: '0.88rem',
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    backgroundColor: '#330D0F',
+    border: '2px solid #330D0F',
+    borderRadius: '0.25rem',
+    cursor: 'pointer',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  table: {
+    width: '100%',
+    borderSpacing: '0.5rem',
+    fontFamily: 'Poppins, sans-serif',
+    tableLayout: 'fixed', // Ensures columns are fixed in width
+    color: '#330D0F'
+  },
+  th: {
+    borderTop: '3px solid #330D0F',
+    borderBottom: '3px solid #330D0F',
+    borderLeft: '3px solid #330D0F',
+    borderRight: '3px solid #fff',
+    backgroundColor: '#4A1416',
+    color: '#FFFFFF',
+    height: '3rem',
+    fontFamily: 'Poppins, sans-serif',
+    overflow: 'hidden', // Hide overflow
+    whiteSpace: 'nowrap', // Prevent text from wrapping
+    textOverflow: 'ellipsis', // Add ellipsis if content is too long
+  },
+  thLast: {
+    borderRight: '3px solid #330D0F',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  td: {
+    border: '3px solid #330D0F',
+    height: '2.8rem',
+    textAlign: 'center',
+    fontFamily: 'Poppins, sans-serif',
+    //wordWrap: 'break-word', // Break long words to fit within the cell
+    overflow: 'hidden', // Hide overflow
+    textOverflow: 'ellipsis', // Add ellipsis if content is too long
+    backgroundColor: '#fff',
+  },
+  operations: {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '1rem',
+    fontFamily: 'Poppins, sans-serif',
+  },
+  icon: {
+    fontSize: '1.5rem',
+    fontFamily: 'Poppins, sans-serif',
+  },
+};
 
+const totalPriceRowStyle = {
+  totalPriceLabel: {
+    border: '3px solid #330D0F',
+    borderRadius: '0.25rem',
+    textAlign: 'right',
+    fontWeight: 'bold',
+    padding: '0.75rem',
+    backgroundColor: '#330D0F',
+    fontFamily: 'Poppins, sans-serif',
+    color: '#FFFFFF',
+  },
+  totalPriceValue: {
+    border: '3px solid #330D0F',
+    borderRadius: '0.25rem',
+    textAlign: 'center',
+    backgroundColor: '#330D0F',
+    padding: '0.75rem',
+    fontWeight: 'bold',
+    fontFamily: 'Poppins, sans-serif',
+    color: '#FFFFFF',
+  },
+  emptyCell: {
+    border: '3px solid #330D0F',
+    borderRadius: '0.25rem',
+    padding: '0.75rem',
+    fontFamily: 'Poppins, sans-serif',
+    backgroundColor: '#fff',
+  },
+};
 
 
 export default PaymentDashboard;
