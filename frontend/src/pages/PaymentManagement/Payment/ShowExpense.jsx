890dbfcd6fcdad14c6e370bc7d0ca12a805642d7
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-07 22:13:30 +0530

    Create ShowExpense.jsx

---
+++ frontend/src/pages/PaymentManagement/Payment/ShowExpense.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../../../components/BackButton';
+import Spinner from '../../../components/Spinner';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../../components/headerfooter/Header';
+import Footer from '../../../components/headerfooter/Footer';
+
+const ShowExpense = () => {
+  
+
+export default ShowExpense;
